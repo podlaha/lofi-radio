5a40bb63e204e826cf8bc8700e43df208994488b
podlaha <podlaha@gmail.com>
2026-04-22 08:14:55 +0000

    Use cookies file for yt-dlp YouTube authentication

--- server/index.js
+++ server/index.js
@@ -72,7 +72,7 @@ const ytCache = new Map() // stationId -> resolved stream URL
 
 function resolveYoutubeUrl(youtubeUrl) {
   return new Promise((resolve, reject) => {
-    execFile(YTDLP, ['-g', '-f', 'bestaudio/best', '--extractor-args', 'youtube:player_client=android', youtubeUrl], { timeout: 30000 }, (err, stdout) => {
+    execFile(YTDLP, ['-g', '-f', 'bestaudio/best', '--cookies', '/app/cookies.txt', youtubeUrl], { timeout: 30000 }, (err, stdout) => {
       if (err) return reject(err)
       const url = stdout.trim().split('\n')[0]
       if (!url) return reject(new Error('yt-dlp returned no URL'))
